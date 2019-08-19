f6ad5695e6f04ea8ffe8c58101640c86f17ae87b
Yx_Zou <storm.yxzou@gmail.com>
2019-08-19 15:15:20 +0800

    基础，插槽的使用

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,29 @@
+/*
+ * @Author: Yx_Zou 
+ * @Description: '' 
+ * @Date: 2019-08-19 13:35:45 
+ * @Last Modified by: Yx_Zou
+ * @Last Modified time: 2019-08-19 14:53:52
+ */
+<template>
+  <li class="item">
+    <input type="checkbox" v-model="checked" />
+    <slot name="item" v-bind="{checked}"></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+  data() {
+    return {
+      checked: false
+    };
+  }
+};
+</script>
+<style scoped>
+.item {
+  color: red;
+}
+</style>
