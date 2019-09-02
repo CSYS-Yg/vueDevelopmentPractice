ce4c1e3cdaca97d0155124852b113ee06c813d5c
Yx_Zou <storm.yxzou@gmail.com>
2019-09-02 15:14:34 +0800

    引入 axios 完成本地 mock 数据；联调

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -5,42 +5,49 @@
 </template>
 
 <script>
-import random from "lodash/random";
+import axios from "axios";
 import Chart from "../../components/Charts.vue";
 export default {
   data() {
     return {
-      chartOption: {
-        title: {
-          text: "ECharts 入门示例"
-        },
-        tooltip: {},
-        legend: {
-          data: ["销量"]
-        },
-        xAxis: {
-          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-        },
-        yAxis: {},
-        series: [
-          {
-            name: "销量",
-            type: "bar",
-            data: [5, 20, 36, 10, 10, 20]
-          }
-        ]
-      }
+      chartOption: {}
     };
   },
   mounted() {
+    this.getChartData();
     this.interval = setInterval(() => {
-      console.log("3000");
-      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
-        () => random(100)
-      );
-      // 重新更新 chartOption
-      this.chartOption = { ...this.chartOption };
+      this.getChartData();
+      // // 重新更新 chartOption
+      // this.chartOption = { ...this.chartOption };
     }, 3000);
+  },
+  methods: {
+    getChartData() {
+      axios
+        .get("/api/dashboard/chart", { params: { ID: 12345 } })
+        .then(response => {
+          this.chartOption = {
+            title: {
+              text: "ECharts 入门示例"
+            },
+            tooltip: {},
+            legend: {
+              data: ["销量"]
+            },
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [
+              {
+                name: "销量",
+                type: "bar",
+                data: response.data
+              }
+            ]
+          };
+        });
+    }
   },
   beforeDestroy() {
     clearInterval(this.interval);
