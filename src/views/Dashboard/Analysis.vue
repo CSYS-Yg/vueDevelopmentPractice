476215d6bc377ed4356203e8f1986957d41e415f
Yx_Zou <storm.yxzou@gmail.com>
2019-09-04 14:39:44 +0800

    aixos 二次封装

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import axios from "axios";
+import request from "../../utils/request";
 import Chart from "../../components/Charts.vue";
 export default {
   data() {
@@ -23,30 +23,32 @@ export default {
   },
   methods: {
     getChartData() {
-      axios
-        .get("/api/dashboard/chart", { params: { ID: 12345 } })
-        .then(response => {
-          this.chartOption = {
-            title: {
-              text: "ECharts 入门示例"
-            },
-            tooltip: {},
-            legend: {
-              data: ["销量"]
-            },
-            xAxis: {
-              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-            },
-            yAxis: {},
-            series: [
-              {
-                name: "销量",
-                type: "bar",
-                data: response.data
-              }
-            ]
-          };
-        });
+      request({
+        url: "/api/dashboard/chart",
+        methods: "get",
+        params: { ID: 12345 }
+      }).then(response => {
+        this.chartOption = {
+          title: {
+            text: "ECharts 入门示例"
+          },
+          tooltip: {},
+          legend: {
+            data: ["销量"]
+          },
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+          },
+          yAxis: {},
+          series: [
+            {
+              name: "销量",
+              type: "bar",
+              data: response.data
+            }
+          ]
+        };
+      });
     }
   },
   beforeDestroy() {
